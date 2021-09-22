f55a8ad513cdc181929cb805d63a254209688158
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-09-22 13:29:37 -0300

    ClientRepository and ClientService created. H2 Database configured

--- backend/src/main/java/com/devsuperior/crudclients/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/crudclients/resources/ClientResource.java
@@ -1,24 +1,26 @@
 package com.devsuperior.crudclients.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.crudclients.entities.Client;
+import com.devsuperior.crudclients.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Joao", "123456", 12000.0, null, 2));
-		list.add(new Client(2L, "Maria", "987654", 18000.00, null, 3));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
 	}
 }
