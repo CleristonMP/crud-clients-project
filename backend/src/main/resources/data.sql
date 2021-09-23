73bbab78efb3a95a7f33acbcee659eab0cf2f970
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-09-23 14:10:21 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Maria', '123456', 13000.00, 3);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Joao', '987654', 12000.00, 1);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Carlos', '456789', 8000.00, 2);
