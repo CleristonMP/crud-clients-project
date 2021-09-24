52133615f231e5e0e48382702a8c4d7a00ef7561
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-09-24 14:22:14 -0300

    Implementation of data for auditing

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_client (name, cpf, income, children) VALUES ('Maria', '123456', 13000.00, 3);
-INSERT INTO tb_client (name, cpf, income, children) VALUES ('Joao', '987654', 12000.00, 1);
-INSERT INTO tb_client (name, cpf, income, children) VALUES ('Carlos', '456789', 8000.00, 2);
+INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
+INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
+INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
