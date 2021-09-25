0e9e93a3b94dd8f5e158ee9e78ad94f382ed6514
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2021-09-25 11:54:35 -0300

    Project completed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,42 +1,120 @@
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Maria', '123456', 13000.00, 3, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Joao', '987654', 12000.00, 1, NOW());
-INSERT INTO tb_client (name, cpf, income, children, created_At) VALUES ('Carlos', '456789', 8000.00, 2, NOW());
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Quinlan Buckley','41686737311',6007.14,11,'1994-03-06 07:17:20'),
+  ('Remedios Rich','80554616562',2142.32,1,'1995-04-02 19:08:16'),
+  ('Michael Sellers','17335483841',4558.80,11,'1986-05-15 19:16:01'),
+  ('Berk Dejesus','39138197320',3163.42,0,'1989-08-21 18:34:05'),
+  ('Lucius Fisher','27185253616',6680.32,6,'1991-03-22 06:15:54'),
+  ('Reed Cameron','29848785806',7109.80,4,'1991-04-30 10:37:35'),
+  ('Sharon Grant','23683588854',4316.80,7,'1990-07-12 05:38:28'),
+  ('Rae Orr','37445846847',7169.99,0,'1986-12-10 18:56:04'),
+  ('Uriel Barton','52085724216',9259.32,3,'1988-05-06 18:31:03'),
+  ('Yoshi Mcmahon','14329765225',9923.28,0,'1994-06-04 14:44:49');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Tasha Velez','45671271099',7071.01,12,'1986-06-18 02:52:46'),
+  ('Sean Richards','03502544253',6534.58,4,'1987-09-11 00:08:31'),
+  ('Rafael Aguilar','34134132184',8652.60,12,'1994-05-08 23:32:18'),
+  ('Yuri Frye','64827374284',1044.95,12,'1990-07-17 13:50:33'),
+  ('Yoko Talley','91043366246',9605.45,9,'1990-07-10 06:37:44'),
+  ('Nasim Leon','76121174855',7974.11,9,'1989-12-02 15:09:03'),
+  ('Phoebe Lopez','31840606455',1772.23,5,'1993-08-09 15:56:40'),
+  ('Tate Richard','66515582777',4425.08,12,'1994-12-16 10:11:10'),
+  ('Halee Padilla','51288268458',3791.09,12,'1990-07-20 07:58:09'),
+  ('Isabelle Jensen','67268498573',7687.36,10,'1991-02-09 05:48:03');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Mari Garcia','14976801562',5772.45,1,'1992-12-06 09:27:04'),
+  ('Noelani Wood','10285255744',1152.95,9,'1991-03-25 17:10:20'),
+  ('Quinn Park','38867764666',9366.98,10,'1993-08-13 15:33:29'),
+  ('Barrett Nichols','21523154337',7201.14,11,'1985-01-04 12:31:28'),
+  ('Daquan Short','62258736267',6306.47,9,'1994-01-13 16:54:51'),
+  ('Alan Beach','67830321301',6968.17,1,'1986-06-29 07:03:52'),
+  ('Nina Figueroa','58377316554',7311.20,9,'1992-11-20 05:44:10'),
+  ('Hiram Williams','84776318059',7289.45,0,'1989-01-31 11:11:10'),
+  ('Griffin Scott','16770876645',6463.49,5,'1990-08-25 13:07:52'),
+  ('Kevyn Brennan','84615106781',5926.65,6,'1992-06-22 02:15:47');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Hashim Bond','07777008316',3900.09,7,'1995-05-14 06:48:10'),
+  ('Ulla Talley','68978798361',9697.68,8,'1985-08-17 22:21:27'),
+  ('Medge Stafford','35138928554',2691.90,0,'1995-11-14 01:27:59'),
+  ('Cody Newman','43112718286',3435.91,5,'1992-09-11 15:55:07'),
+  ('Lamar Hopper','20038977228',6339.56,9,'1988-11-30 01:58:20'),
+  ('Cade Washington','86209661068',1134.51,10,'1991-01-27 07:48:06'),
+  ('Declan Schmidt','39141655855',8660.80,11,'1988-06-26 02:55:30'),
+  ('Cassady Bradley','63681165784',3077.10,12,'1991-06-27 18:08:44'),
+  ('Kim Porter','62736358136',2075.53,3,'1993-10-01 05:12:23'),
+  ('Ignatius Cantrell','76762509132',5100.26,4,'1993-03-08 18:23:18');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Guy Gordon','48616916967',2873.01,0,'1994-08-14 21:34:11'),
+  ('Aspen Hooper','45670492440',2300.18,2,'1992-03-26 02:49:49'),
+  ('Tyler Flores','63602156653',1059.75,3,'1991-07-15 03:04:53'),
+  ('James Kramer','44455946802',2904.77,2,'1990-01-14 18:57:40'),
+  ('Benedict Holder','72456967884',8603.81,6,'1993-04-17 20:38:06'),
+  ('Lacey Drake','72766068154',6920.20,6,'1992-02-15 17:21:13'),
+  ('Amery Brown','73557889735',1097.05,9,'1990-07-27 02:16:33'),
+  ('Bruno Hawkins','43451112265',5480.33,1,'1994-02-08 04:23:08'),
+  ('Laurel Gross','06884875247',9959.50,6,'1994-01-17 19:06:37'),
+  ('Harper Hess','83342161566',6502.03,2,'1992-01-09 07:24:49');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Nina Bentley','57841851265',5933.92,11,'1993-09-22 23:20:16'),
+  ('Kadeem Dennis','76614638661',9201.97,12,'1987-12-29 06:39:33'),
+  ('Adara Gross','26438484318',1737.77,9,'1989-03-02 14:08:45'),
+  ('Brent Buck','42066549655',5047.79,8,'1994-11-04 19:23:54'),
+  ('Gail Serrano','14862553170',9705.96,9,'1993-02-01 17:02:38'),
+  ('Malik Mckay','85148768589',4016.05,3,'1995-03-07 23:52:56'),
+  ('Sasha Cross','77242009753',7927.65,5,'1989-07-20 21:55:57'),
+  ('Georgia Callahan','11241388185',7554.90,11,'1986-11-07 21:38:23'),
+  ('Isaiah Madden','14230406374',5179.65,0,'1993-05-21 08:18:03'),
+  ('Kieran Barton','93916649750',4592.19,11,'1991-10-31 10:49:39');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Yoshio Carter','98666748186',1124.63,3,'1992-07-22 12:11:22'),
+  ('Dana Marks','53241131334',7548.64,8,'1993-08-27 18:24:12'),
+  ('Deborah Short','69236341586',7221.94,7,'1990-06-26 02:55:33'),
+  ('Darius Stuart','41024111262',4664.43,1,'1992-02-27 08:54:38'),
+  ('Slade Anderson','71736310742',1597.15,10,'1989-02-03 11:37:19'),
+  ('Harper Flynn','73503833966',5875.52,4,'1986-06-15 19:54:51'),
+  ('Joel Valencia','28690454363',2314.42,11,'1991-08-03 00:39:43'),
+  ('Akeem Wright','89713810205',6715.95,6,'1989-09-08 05:56:48'),
+  ('Harper Bean','78223538242',5391.78,10,'1991-07-14 17:09:35'),
+  ('Ahmed Stone','49613124563',1596.89,6,'1990-07-18 02:04:47');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Jacob Compton','61524680403',9636.17,12,'1994-05-14 20:49:39'),
+  ('Ebony Wyatt','36625646134',4442.42,12,'1995-04-18 00:37:21'),
+  ('Aileen Kinney','43835603758',7490.77,12,'1991-09-17 01:53:26'),
+  ('Beck Buchanan','13205124840',6462.32,8,'1989-07-04 10:54:49'),
+  ('Oliver Hays','84045843267',9944.45,3,'1990-11-30 13:42:50'),
+  ('Kelly Buck','26866715171',9520.04,5,'1993-09-15 14:37:50'),
+  ('Winter Casey','93873410550',9179.39,8,'1985-07-04 09:06:34'),
+  ('Demetrius Knapp','47534838317',6157.56,3,'1990-05-10 02:17:01'),
+  ('Amethyst Boone','49929824588',9982.86,0,'1989-06-18 23:51:06'),
+  ('Kylee Moreno','25567802659',9877.01,5,'1986-09-17 18:35:11');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Kylynn Martinez','65975604147',1693.83,2,'1994-12-11 08:01:50'),
+  ('Adam Franklin','39569275106',6057.06,1,'1985-07-12 00:04:00'),
+  ('Wynne Hodges','78187654277',7217.60,10,'1987-10-08 07:59:14'),
+  ('Hedda Roberson','98276287886',8782.41,8,'1995-09-08 06:14:26'),
+  ('Hollee Hopkins','36639383311',1789.37,3,'1993-10-05 11:33:01'),
+  ('Brent Pugh','58367148376',8852.00,9,'1990-08-24 15:27:11'),
+  ('Holly Hart','38277292390',3069.37,8,'1995-02-22 12:16:21'),
+  ('Lavinia Logan','35756206628',6010.46,6,'1993-07-01 15:21:04'),
+  ('Vera Weaver','40177737475',7458.65,6,'1988-10-20 20:30:16'),
+  ('Kyra Woodard','58577347426',1477.74,1,'1989-04-08 23:52:31');
+INSERT INTO tb_client (name,cpf,income,children,birth_Date)
+VALUES
+  ('Priscilla Ramirez','31550232731',6943.43,4,'1988-10-18 14:12:11'),
+  ('Lenore Zimmerman','21772450355',2933.10,2,'1992-08-31 21:28:17'),
+  ('Kenneth Delacruz','67892559418',9641.45,12,'1985-11-01 12:06:08'),
+  ('Berk Montgomery','11418068862',5281.01,6,'1990-11-24 07:22:41'),
+  ('Fitzgerald Cameron','59375115033',5332.08,1,'1985-09-26 09:32:26'),
+  ('Zorita Sweet','74882371719',3991.92,4,'1985-04-13 09:36:31'),
+  ('Gareth Eaton','81274763481',9375.96,9,'1994-12-17 01:02:57'),
+  ('Nathaniel Miles','82437116511',4233.24,2,'1995-11-26 19:17:25'),
+  ('Ira Calderon','39206777641',7667.28,4,'1989-09-19 23:12:00'),
+  ('Cheyenne Melendez','69468612372',5288.44,0,'1992-05-13 04:09:53');
